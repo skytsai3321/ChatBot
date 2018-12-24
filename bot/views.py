65d0984936e4547d6e43115a7c9daa3b8e0eb6ab
Leo Tsai <leotsai@easpnet.com>
2018-12-24 16:43:03 +0800

    make it better

---
+++ bot/views.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+from linebot import LineBotApi, webhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage
+
+# Create your views here.
+line_bot_api = LineBotApi(settings.Line_CHANNEL_ACCESS_TOKEN)
+parser = webhookParser(settings.Line_CHANNEL_SECRET)
+
+@csrf_exempt
+def callback(request):
+
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+
+        try:
+            events = parser.parser(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+        
+        for event in events:
+            if isinstance(event, MessageEvent):
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
